683a434ddea54f5570da95893cfc4d25622cf14e
Francisco Melicias <melicias1999@gmail.com>
2023-04-26 18:25:23 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ const app = createApp(App)
 
 const socketIO = new VueSocketIO({
   debug: true,
-  connection: typeof process.env.VITE_SOCKETSIP !== 'undefined' ? "http://" + process.env.VITE_SOCKETSIP + ":3000" : "http://" + import.meta.env.VITE_SOCKETSIP + ":3000",
+  connection: typeof process.env.VITE_SOCKETSIP !== 'undefined' ? "http://" + process.env.VITE_SOCKETSIP + ":3001" : "http://" + import.meta.env.VITE_SOCKETSIP + ":3001",
  })
 
 app.config.globalProperties.$serverUrl = "http://127.0.0.1:8000/";
